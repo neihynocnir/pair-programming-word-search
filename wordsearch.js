a5056d6ef797b329acbdb734b6b9a64050c9c831
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-08 22:22:08 +0000

    Pair programing exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        } else {
+            for (let str of verticalJoin) {
+                if (str.includes(word)) {
+                    return true;
+                }
+            }
+        }
     }
+    return false;
 }
+
+const transpose = function(letters) {
+    let result = [];
+    for (let x = 0; x < letters.length; x ++) {
+      for (let y = 0; y < letters[x].length; y++) {
+        if (!result[y]) {
+          result[y] = [];
+        }
+        result[y].push(letters[x][y]);
+      }
+    }
+    return result;
+  };
 
 module.exports = wordSearch
